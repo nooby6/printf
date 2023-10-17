d2f545c864332193c2349f3b12700d448b386cda
nooby6 <giftaudrine1@gmail.com>
2023-10-17 10:27:19 -0700

    Updates

---
+++ _printf_test.c
@@ -0,0 +1,20 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+#include <stdarg.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int chars_printed;
+	/*Call _printf to get the count of characters printed*/
+	chars_printed = _printf("Code for Samuel and Whrite printf project.");
+	/*Print the count using printf*/
+	printf("Total characters printed: %d\n", chars_printed);
+	printf("%c\n", "P");/*Print character P using %c*/
+	printf("%s\n", "group_project");/*Print a string using %s*/
+	return (0);
+}
---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int len;
+    unsigned int ui;
+    void *addr;
+
+    len = _printf("Audrine and Samuel custom printf!\n");
+    ui = (unsigned int)1024;
+    addr = (void *)0x7ffe637541f0;
+
+    _printf("Length:[%d]\n", len);
+    _printf("Unsigned:[%u]\n", ui);
+    _printf("Unsigned octal:[%o]\n", ui);
+    _printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "This is a test string.");
+    _printf("Address:[%p]\n", addr);
+    _printf("Percent:[%%]\n");
+
+    return (0);
+}
